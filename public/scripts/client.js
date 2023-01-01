318eeae39c134f3c83b7d49582258638c072788c
Tammy-Lee <tammy-lee.plamondon@telus.com>
2023-01-01 05:50:31 -0800

    fixing all errors

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,39 +4,35 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function() {
-
-
-
+$(document).ready(function () {
   // Fake data taken from initial-tweets.json
   const data = [
     {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
+      user: {
+        name: "Newton",
+        avatars: "https://i.imgur.com/73hZDYK.png",
+        handle: "@SirIsaac",
       },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
+      content: {
+        text: "If I have seen further it is by standing on the shoulders of giants",
       },
-      "created_at": 1461116232227
+      created_at: 1461116232227,
     },
     {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
+      user: {
+        name: "Descartes",
+        avatars: "https://i.imgur.com/nlhLi3I.png",
+        handle: "@rd",
       },
-      "content": {
-        "text": "Je pense , donc je suis"
+      content: {
+        text: "Je pense , donc je suis",
       },
-      "created_at": 1461113959088
-    }
+      created_at: 1461113959088,
+    },
   ];
 
   /* Your code for creating the tweet element */
-  const renderTweets = function(tweets) {
+  const renderTweets = function (tweets) {
     // loops through tweets
     for (let tweet of tweets) {
       // calls createTweetElement for each tweet
@@ -44,17 +40,15 @@ $(document).ready(function() {
       // takes return value and appends it to the tweets container
       $("#tweet-container").prepend(createATweet);
     }
-
-
   };
 
-  const escape = function(str) {
+  const escape = function (str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
 
-  const createTweetElement = function(tweet) {
+  const createTweetElement = function (tweet) {
     let $tweet = `
 <article class="tweet">
   <header>
@@ -79,31 +73,26 @@ $(document).ready(function() {
     return $tweet;
   };
 
-  // ajax GET request 
-  const loadTweets = function() {
-    $.ajax('/tweets', { method: 'GET' })
-      .then(function(tweets) {
-        console.log("The tweets are storing in the database");
-        renderTweets(tweets);
-      });
-    $('#tweeter-text').val("");
-    $('.counter').text(140);
+  // ajax GET request
+  const loadTweets = function () {
+    $.ajax("/tweets", { method: "GET" }).then(function (tweets) {
+      renderTweets(tweets);
+    });
+    $("#tweeter-text").val("");
+    $(".counter").text(140);
   };
 
-
-  const $form = $('form');
-  $form.on('submit', function(events) {
+  const $form = $("form");
+  $form.on("submit", function (events) {
     events.preventDefault();
-    console.log('Button clicked, performing ajax call...');
+
     const tweet = $(this).serialize();
-    console.log(tweet);
+
     if (tweet.length === 5) {
-      return $('#error-message').slideDown(400);
-    } else if (tweet.length >= 146)
-      return $('#error-two').slideDown(400);
-    $.ajax('/tweets', { method: 'POST', data: tweet }).then(loadTweets());
+      return $("#error-message").slideDown(400);
+    } else if (tweet.length >= 146) return $("#error-two").slideDown(400);
+    $.ajax("/tweets", { method: "POST", data: tweet }).then(loadTweets());
   });
-  
 
   loadTweets();
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,10 @@
 $(document).ready(function() {
   // --- our code goes here ---
-  console.log("Say Hello to my Little Friend");
   $("#tweeter-text").on("input", function() {
     const maxLimit = 140;
     let characterCount = $(this).val().length;
     let textFromUser = $(this).val();
     let remaining = maxLimit - characterCount;
-    console.log(remaining);
     const safeHTML = `<p>${escape(textFromUser)}</p>`;
 
     let $counterButton = $(this).parent().find(".counter");
